d9659f0a9e40fb883d175288156c5a8ea1f07a73
leandro <leandrovilarinho2002@gmail.com>
2024-12-28 03:36:45 -0300

    Criação de Middleware e CRUD

--- index.js
+++ index.js
@@ -1,18 +1,80 @@
-const express = require('express');
+const express = require('express')
 
-const server = express();
+const server = express()
+
+server.use(express.json())
 
 // Query params = ?nome=NodeJS
 // Route params = /curso/2
 // Request Body = { nome: 'NodeJS', tipo: 'Backend'}
 
-const cursos = ['Node JS', 'Java script', 'Reactive Native'];
+// CRUD Create, Read, Update, Delete
+
+const cursos = ['Node JS', 'Java script', 'Reactive Native']
+
+// Middleware global
+server.use((req, res, next) => {
+  console.log(`URL CHAMADA: ${req.url}`)
+
+  return next()
+})
+
+function checkCurso(req, res, next){
+  if(!req.body.name){
+    return res.status(400).json({ error: "Nome do curso é obrigatório"})
+  }
+
+  return next();
+}
+
+function checkIndexCurso(req, res, next){
+  const curso = cursos[req.params.index]
+
+  if(!curso){
+    return res.status(400).json({ error: "O curso requisitado não existe"})
+  }
+
+  req.curso = curso
+
+  return next()
+}
+
+server.get('/cursos', (req, res) => {
+  return res.json(cursos)
+})
 
 // localhost:3000/curso
-server.get('/curso/:index', (req, res) => {
-  const index = req.params.index;
+server.get('/cursos/:index', checkIndexCurso, (req, res) => {
+  const index = req.params.index
 
-  return res.json(cursos[index]);
+  return res.json(req.curso)
+})
+
+// Criando um novo curso
+server.post('/cursos', checkCurso, (req, res) => {
+  const { name } = req.body
+  cursos.push(name)
+
+  return res.json(cursos)
+})
+
+// Atualizando um curso
+server.put('/cursos/:index', checkCurso, checkIndexCurso, (req,res) => {
+  const { index } = req.params
+  const { name } = req.body
+
+  cursos[index] = name
+
+  return res.json(cursos)
+})
+
+// Excluindo alguns cursos
+
+server.delete('/cursos/:index', checkIndexCurso, (req,res) => {
+  const { index } = req.params
+  cursos.splice(index)
+
+  return res.json(cursos)
 })
 
 server.listen(3000);
